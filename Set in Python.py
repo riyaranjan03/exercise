152af2b392b226afb47d113c871a50bb67de3a45
riyaranjan03 <126847783+riyaranjan03@users.noreply.github.com>
2023-04-11 17:08:37 +0530

    Update Set in Python.py

--- Set in Python.py
+++ Set in Python.py
@@ -2,7 +2,7 @@ sentence= "Hey I am walking here I am walking here o captain my captain water wa
 sl=sentence.split()
 print("sentence_list= ",sl,"\n")
 
-ss=set(sl)
-print("sentence_set= ",ss,"\n")
+s=set(sl)
+print("sentence_set= ",s,"\n")
 
-print("num_unique: ",len(ss),"\n")
+print("num_unique: ",len(s),"\n")
