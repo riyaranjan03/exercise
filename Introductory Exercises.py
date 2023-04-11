a0a88b0309260e4a5bceb2d015bad6a56ec9dd22
riyaranjan03 <126847783+riyaranjan03@users.noreply.github.com>
2023-04-11 20:57:28 +0530

    Update Introductory Exercises.py

--- Introductory Exercises.py
+++ Introductory Exercises.py
@@ -14,3 +14,8 @@ print("Remainder is:",mod)
 floor =a//b
 print("Floor division is:",floor)
 
+#conversion from f to c:
+f=float(input("how much is the temperature "))
+c=(f-32)*(5/9)
+print(" temperature in degree celcius is, " c)
+
