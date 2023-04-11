4f03720348442e897fb4c4276022dec32ec22873
riyaranjan03 <126847783+riyaranjan03@users.noreply.github.com>
2023-04-11 16:07:30 +0530

    Update and rename Set in Python to Set in Python.py

---
+++ Set in Python.py
@@ -0,0 +1,8 @@
+sentence= "Hey I am walking here I am walking here o captain my captain water water everywhere nor a drop to drink"
+sl=sentence.split()
+print("sentence_list= ",sl,"\n")
+
+ss=set(sl)
+print("sentence_set= ",ss,"\n")
+
+print("num_unique: ",len(ss),"\n")
