b1ef6292035991a98c2fb4b52d1a91ae9449e6b1
riyaranjan03 <126847783+riyaranjan03@users.noreply.github.com>
2023-04-11 15:52:19 +0530

    Update and rename Advance Scripting to Advance Scripting.py

---
+++ Advance Scripting.py
@@ -0,0 +1,6 @@
+a=int(input())
+for i in range(0,a):
+    name=input()
+    adm_id=input()
+    cap=int(input())
+    print( "Hi {0},\n\n Congratulations...!  \n {0} You got selected for next round of recruitment process \n submit your academic credential including primary and secondary certificates. \n Your admission ID is {1} and will be eligible \n for the next round of interview with a CAP rank of {2} \n If you submit all the documents on time and process university might offer you a scholarship. \n\n".format(name,adm_id,cap))
