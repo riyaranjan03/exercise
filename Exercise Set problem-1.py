c3f18f20ccbfeaf90462b8645cc7d6de666a6a30
riyaranjan03 <126847783+riyaranjan03@users.noreply.github.com>
2023-04-11 15:46:53 +0530

    Update Exercise Set problem-1.py

--- Exercise Set problem-1.py
+++ Exercise Set problem-1.py
@@ -1,1 +1,11 @@
-
+def mem(n,a):
+    d1=(n)*(15/100)
+    d2=(n)*(8/100)
+    if a=="yes":
+        return (n-(d1+d2))
+    else:
+       return (n-d2)
+    
+n=int(input('cost of your item: '))
+a=input('are you a prime member? yes or no: ')
+print(mem(n,a))
