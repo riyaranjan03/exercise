90eae100e67f1b0bb60559a0146cdaf010b6ba78
riyaranjan03 <126847783+riyaranjan03@users.noreply.github.com>
2023-04-11 17:06:32 +0530

    Update and rename Introductory Exercises to Introductory Exercises.py

---
+++ Introductory Exercises.py
@@ -0,0 +1,16 @@
+#calculation on two numbers
+a =float(input())
+b =float(input())
+sum =a+b
+print("sum is:",sum)
+dif =a-b
+print("The difference is:",dif)
+prod =a*b
+print("Multiplication is:",prod)
+div =a/b
+print("Division is:",div)
+mod =a%b
+print("Remainder is:",mod)
+floor =a//b
+print("Floor division is:",floor)
+
